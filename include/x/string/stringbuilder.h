cfffd01300531a4d9816ccfa5846f5a777d25a70
hypeaches <hypeaches@live.cn>
2018-08-02 08:31:54 +0800

    init project

---
+++ include/x/string/stringbuilder.h
@@ -0,0 +1,16 @@
+#ifndef LIBX_STRING_STRINGBUILDER_H
+#define LIBX_STRING_STRINGBUILDER_H
+
+namespace x {
+
+class stringbuilder {
+public:
+    stringbuilder();
+    stringbuilder(int fmtlen);
+
+    void test();
+};
+
+}
+
+#endif // LIBX_STRING_STRINGBUILDER_H
---
+++ src/string/stringbuilder.cpp
@@ -0,0 +1,19 @@
+#include "x/string/stringbuilder.h"
+#include <cstdio>
+
+namespace x {
+
+stringbuilder::stringbuilder()
+{
+}
+
+stringbuilder::stringbuilder(int fmtlen)
+{
+}
+
+void stringbuilder::test()
+{
+    printf("abc\n");
+}
+
+}
